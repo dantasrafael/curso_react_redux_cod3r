d853c221cc24066816f0112b54169a18a952efa6
Rafael Dantas <rafael.dantas@omniti.com.br>
2020-08-21 12:04:22 -0300

    alterando porta

--- src/config/server.js
+++ src/config/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
-const port = 3003
+const port = process.env.PORT ? process.env.PORT : 3003
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
