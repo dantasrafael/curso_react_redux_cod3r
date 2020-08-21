82b6d931a0d9e4b3db5a0bfac50131a5ec6731f3
Rafael Dantas <rafael.dantas@omniti.com.br>
2020-08-21 13:16:23 -0300

    ajustes para deploy frontend

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
 export default {
     API_URL: 'https://dantas-my-money-app-backend.herokuapp.com/api',
-    OAPI_URL: 'http://dantas-my-money-app-backend.herokuapp.com/oapi'
+    OAPI_URL: 'https://dantas-my-money-app-backend.herokuapp.com/oapi'
 }
